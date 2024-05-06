c7c06a7d154eecbe2acf67ffada6157893fd8d2e
Rahad-Ullah <myemail.abc20@gmail.com>
2024-05-06 17:46:49 +0600

    Generic

---
+++ module-2/2.3 Generic.ts
@@ -0,0 +1,42 @@
+{
+    //* Generic Type
+    type GenericArray<T> = Array<T>
+    
+
+    // const rollNumbers: number[] = [3, 5, 7]
+    const rollNumbers: GenericArray<number> = [3, 5, 7]
+
+    // const mentors: string[] = ['Rahad', 'Noman', 'Tuhin']
+    const mentors: Array<string> = ['Rahad', 'Noman', 'Tuhin']
+
+    // const boolArray: boolean[] = [true, false, true]
+    const boolArray: GenericArray<boolean> = [true, false, true]
+
+    const users: GenericArray<{name: string, age: number}> = [
+        {
+            name: 'Rahad',
+            age: 25,
+        },
+        {
+            name: 'Noman',
+            age: 21,
+        }
+    ]
+
+
+
+    // * Generic Touple
+    type GenericTouple<X, Y> = [X, Y]
+
+    const persons: GenericTouple<string, string> = ['Mr. X', 'Mr. Y']
+
+    const usersWithId: GenericTouple<number, {name: string, age: number}> = [
+        101,
+        {
+            name: 'Rahad',
+            age: 25
+        }
+    ]
+    
+    
+}
