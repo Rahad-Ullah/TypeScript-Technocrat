c786da7fc64ab5aca4956bda40d5d10ed87e52a6
Rahad-Ullah <myemail.abc20@gmail.com>
2024-05-07 12:36:33 +0600

    Generic constraint with Keyof operator

---
+++ module-2/2.7 Keyof operator.ts
@@ -0,0 +1,33 @@
+{
+    //* Generic constraint with keyof operator
+
+    type Vehicle = {
+        bike: string;
+        car: string;
+        ship: string;
+    }
+
+    type Owner = "bike" | "car" | "ship"    // manually
+
+    type Owner2 = keyof Vehicle;    // dynamically
+
+
+    // Implement: make a function that return it's specific value
+    const getPropertyValue = <O, K extends keyof O>(obj: O, key: K) => {
+        return obj[key];
+    }
+
+    const user = {
+        name: 'Rahad',
+        age: 21,
+        address: 'Sylhet',
+    }
+    const result1 = getPropertyValue(user, 'name')
+
+    const car = {
+        brand: 'Toyota',
+        model: 'F35'
+    }
+    const result2 = getPropertyValue(car, 'brand')    
+    
+}
