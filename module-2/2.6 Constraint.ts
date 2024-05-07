103d72dc8f8d13f17d6f85878ba5b0c5a277480b
Rahad-Ullah <myemail.abc20@gmail.com>
2024-05-07 11:54:42 +0600

    Constraint

---
+++ module-2/2.6 Constraint.ts
@@ -0,0 +1,34 @@
+{
+    //* Constraint
+
+    const addCourseToStudent = <T extends {
+        id: number;
+        name: string;
+        email: string;
+    }>(student: T) => {
+        const course = 'Next Level Web Development'
+        return {
+            ...student,
+            course,
+        }
+      }
+    
+      const student1 = addCourseToStudent<{
+        id: number;
+        name: string;
+        email: string;
+      }>({
+        id: 101,
+        name: 'Noman',
+        email: 'x@gmail.com'
+      })
+
+
+      const student2 = addCourseToStudent({
+        id: 102,
+        name: 'Tuhin',
+        email: 'y@mail.com',
+        emni: 'emni'
+      })
+    
+}
